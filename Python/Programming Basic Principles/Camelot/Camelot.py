85ee89f21743e7e49ba3b866e1a39dd800ba9798
Bfluegge-SNHU <brandon.fluegge@snhu.edu>
2025-10-28 00:29:42 -0700

    Update Camelot.py
    Finalized player class creation
    
    Fixed bug with focus selection
    
    Added os.system to clear terminal after section is completed.

--- Python/Programming Basic Principles/Camelot/Camelot.py
+++ Python/Programming Basic Principles/Camelot/Camelot.py
@@ -1,20 +1,40 @@
 import pygame
-from characterCreate import playerInit, focusSelection, classSelection
+import os
+from characterCreate import playerInit, focusSelection, classSelection, playerClass
 
 #Player initialization
 player = playerInit.playerInit()
+os.system('cls')
 
 #Class choice
 player = classSelection.characterClass(player)
+os.system('cls')
 
 #focus choice
-player = focusSelection.focusSelection(player['class'], player)
+player = focusSelection.focusSelection(player['Class'], player)
+os.system('cls')
 
+#Final creation of player object
+playerOne = playerClass.Player(
+    player['name'], 
+    player['inv'], 
+    player['spells'], 
+    player['Class'], 
+    player['focus'], 
+    player['Gender'], 
+    player['Age'], 
+    {'gold' : player['Money']['gold'], 'silver' : player['Money']['silver'], 'copper' : player['Money']['copper']}
+    )
 
-print(player['name'] + ": " + player['class'] + "\nFocus: " + player['focus'] + "\n")
-print("Inventory: \n")
-print(player['inv'])
+os.system('cls')
+print(playerOne.name + " The " + playerOne.Class + "\nFocus: " + playerOne.focus + "\n")
+print("Inventory: \n" + str(playerOne.inv))
+print("Gold: " + str(playerOne.Money['gold']) + " Silver: " + str(playerOne.Money['silver']) + " Copper: " + str(playerOne.Money['copper']))
 print("*" * 20)
-print("Spells: \n")
-print(player['spells'])
+print("Spells: \n" + str(playerOne.spells))
 print("*" * 20)
+confirm = input("Does this information look correct? [y/n] > ")
+
+if confirm == 'y' or confirm == 'Y' or confirm == 'yes' or confirm == 'Yes':
+    print("\n\nWelcome to Camelot! \n\nHome of the knights of the round table! \n\nWhere your story has yet to unfold...\n")
+    input("Press Enter to Continue...")
