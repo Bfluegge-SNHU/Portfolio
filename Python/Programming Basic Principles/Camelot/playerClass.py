268dd3578e4f9358b6920439fcd4a37e70ec889f
Bfluegge-SNHU <brandon.fluegge@snhu.edu>
2025-10-28 00:33:19 -0700

    Create playerClass.py

---
+++ Python/Programming Basic Principles/Camelot/playerClass.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, inv, spells, Class, focus, Gender, Age, Money):
+        self.name = name
+        self.inv = inv
+        self.spells = spells
+        self.Class = Class
+        self.focus = focus
+        self.Gender = Gender
+        self.Age = Age
+        self.Money = Money
