3369d6684f2dae4b045efb5ec899001b4fb0a765
Bfluegge-SNHU <brandon.fluegge@snhu.edu>
2025-10-28 00:31:36 -0700

    Update focusSelection.py
    Bug Fix: first conditional statement under Ranger class considered 'Truthy'. FIXED

--- Python/Programming Basic Principles/Camelot/characterCreate/focusSelection.py
+++ Python/Programming Basic Principles/Camelot/characterCreate/focusSelection.py
@@ -1,37 +1,45 @@
 #Focuses are subcategories for player classes
-def focusSelection(Class, player):
-    #Ranger focuses
-    if Class == "Ranger":
-        focusOptions = {'Thief', 'Survivalist'}
-        focus = input("Select your focus:\n 1.) Thief\n 2.) Survivalist\n")
+import os
 
-        if focus == '1' or 'Thief' or 'thief':
-            print("")
-            player['focus'] = 'Thief'
-        elif focus == '2' or 'Survivalist' or 'survivalist':
-            player['focus'] = 'Survivalist'
+def focusSelection(Class, player):
+    selected = False
+    while selected == False:
+        os.system('cls')
+    #Ranger focuses
+        if Class == "Ranger":
+            focus = input("Select your focus:\n 1.) Thief\n 2.) Survivalist\n(Enter 1 or 2 on the keyboard)\n")
+
+            if focus in('1', 'theif', 'Theif'):
+                player['focus'] = 'Thief'
+                selected = True
+            elif focus in ('2', 'survivalist', 'Survivalist'):
+                player['focus'] = 'Survivalist'
+                selected = True
+            else:
+                print("Incorrect Selection")
+                continue
                 
 
             
 
     #Barbarian focuses
-    elif Class == "Barbarian":
-        focusOptions = {'berserker', 'beast'}
+        elif Class == "Barbarian":
+            focusOptions = {'berserker', 'beast'}
 
     #Healer focuses
-    elif Class.name == "Healer":
-        focusOptions = {'shaman', 'druid'}
+        elif Class.name == "Healer":
+            focusOptions = {'shaman', 'druid'}
 
     #Mage focuses
-    elif Class.name == "Mage":
-        focusOptions = {'necromancer', 'wizard/witch'}
+        elif Class.name == "Mage":
+            focusOptions = {'necromancer', 'wizard/witch'}
 
     #Paladin focuses
-    elif Class.name == "Paladin":
-        focusOptions = {'Crusader', 'Demonologist'}
+        elif Class.name == "Paladin":
+            focusOptions = {'Crusader', 'Demonologist'}
                             
     #Incorrect selection
-    else:
-        print("Incorrect class")
+        else:
+            print("Incorrect class")
 
     return player
